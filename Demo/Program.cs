b849fceaa0d0dd55136d7d4cdd239b104c293381
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-09 21:49:31 +0200

    Part 02 Functions - ValueType Parameters

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -43,6 +43,33 @@
 
         #endregion
 
+        #region Part 02 Functions - ValueType Parameters
+
+        #region Passing By Value [input parameters]
+
+        static void Swap01(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        #endregion
+
+        #region Passing by ref [Reference] [input output parameters]
+
+        static void Swap02(ref int x,ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        #endregion
+
+
+        #endregion
+
         static void Main(string[] args)
         {
 
@@ -76,6 +103,39 @@
             //Console.WriteLine(folderPath02);//D:\TECK-COURSES\Route-Courses\Back-End(.NET)\ENG-NASR\02 C#\01 Basics\Session 05 {NEW}\Videos
 
             #endregion
+
+            #endregion
+
+            #region Part 02 Functions - ValueType Parameters
+
+            #region Passing By Value [input parameters]
+
+            //int x = 5, y = 10;
+
+            //Console.WriteLine("====== Before Swapping =======");
+            //Console.WriteLine($"x = {x}, y = {y}");//x = 5, y = 10
+
+            //Swap01(x, y);
+
+            //Console.WriteLine("====== After Swapping =======");
+            //Console.WriteLine($"x = {x}, y = {y}");//x = 5, y = 10 
+
+            #endregion
+
+            #region Passing by ref [Reference] [input output parameters]
+
+            //int x = 5, y = 10;
+
+            //Console.WriteLine("====== Before Swapping =======");
+            //Console.WriteLine($"x = {x}, y = {y}");//x = 5, y = 10
+
+            //Swap02(ref x,ref y);
+
+            //Console.WriteLine("====== After Swapping =======");
+            //Console.WriteLine($"x = {x}, y = {y}");//x = 10, y = 5
+
+            #endregion
+
 
             #endregion
 
